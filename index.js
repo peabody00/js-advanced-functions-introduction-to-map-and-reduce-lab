9a040545f6e8b9e799e4e7a1394eabaaa282ab5b
Charlie <peabody00@gmail.com>
2021-06-10 12:14:44 -0500

    commit tree e52aa496e8a330428a21afb498ff99ac44dda67c

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    let reverse = [];
+    sourceArray.forEach(num => {
+     reverse.push(num * -1)
+    })
+    return reverse;
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = sourceArray;
+    return newArray;
+}
+
+function mapToDouble(sourceArray) {
+    let double = [];
+    sourceArray.forEach(num => {
+     double.push(num * 2)
+    })
+    return double;
+}
+
+function mapToSquare(sourceArray) {
+    let square = [];
+    sourceArray.forEach(num => {
+     square.push(num ** 2)
+    })
+    return square;
+}
+
+function reduceToTotal(sourceArray, startPoint=0) {
+    sourceArray.forEach(num => {
+        startPoint += num;
+    })
+    return startPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) return false;
+    }
+    return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) return true;
+    }
+    return false;
+}
